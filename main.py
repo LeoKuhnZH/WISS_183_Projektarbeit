fb32fb109b8965008c6f25c44902dc0fd2b65df8
LeoKuhnZH <leo.kuhn.zh@gmail.com>
2026-05-28 16:00:38 +0200

    made the code runnable

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from encryption.encryption import encrypter_xor
+import sys
+
+
+if __name__ == "__main__":
+    # Check if the user provided enough arguments
+    if len(sys.argv) != 3:
+        print("Usage: ./file.py <data> <key>")
+        sys.exit(1)
+
+    data_in = sys.argv[1]
+    key_in = sys.argv[2]
+
+    encrypter_xor(string=data_in, key=key_in)
