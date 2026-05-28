63efe375505870809ea3a33f539793b88da0dbb2
LeoKuhnZH <leo.kuhn.zh@gmail.com>
2026-05-28 16:11:16 +0200

    fixed the "megalist" bug

--- encryption/encrypt_xor_encryption.py
+++ encryption/encrypt_xor_encryption.py
@@ -2,4 +2,4 @@ from encryption.encrypt_single import encrypt_single_xor
 
 
 def encrypt_xor_encryption(string: str, key: str) -> list[list[int]]:
-    return [encrypt_single_xor(cha, keypiece) for cha in string for keypiece in key]
+    return [encrypt_single_xor(cha, key) for cha in string]
