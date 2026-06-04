96a95f4029ba66068c9cc51689795c3bfc833487
LeoKuhnZH <leo.kuhn.zh@gmail.com>
2026-06-04 13:28:39 +0200

    added option to write to file

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
-from encryption.encryption import encrypter_xor
 import sys
 
+from encryption.encryption import encrypter_xor
 
 if __name__ == "__main__":
     # Check if the user provided enough arguments
-    if len(sys.argv) != 3:
+    if len(sys.argv) == 3 or len(sys.argv) == 4:
         print("Usage: ./file.py <data> <key>")
         sys.exit(1)
 
     data_in = sys.argv[1]
     key_in = sys.argv[2]
 
-    print(encrypter_xor(string=data_in, key=key_in))
+    if len(sys.argv) == 3:
+
+        print(encrypter_xor(string=data_in, key=key_in))
+
+    elif len(sys.argv) == 4:
+
+        output_path = sys.argv[3]
+
+        with open(output_path, "wb") as binary_file:
+            binary_file.write(encrypter_xor(string=data_in, key=key_in).encode('latin-1'))
