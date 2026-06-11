5e0e85bdfcec869142dc14e48ffba9237f44fe4a
LeoKuhnZH <leo.kuhn.zh@gmail.com>
2026-06-11 14:53:02 +0200

    extracted the method that checks whether the input is a filepath

--- xor_main.py
+++ xor_main.py
@@ -4,6 +4,18 @@ import sys
 
 from xor_encryption.encryption import encrypter_xor
 
+def _find_string(input_string: str) -> str:
+    if os.path.isfile(input_string):
+        try:
+            with open(input_string, "r", encoding="utf-8") as f:
+                return f.read()
+        except Exception as e:
+            print(f"Error reading file '{input_string}': {e}")
+            sys.exit(1)
+
+    else:
+        return input_string
+
 if __name__ == "__main__":
     # Check if the user provided enough arguments
     if len(sys.argv) != 3 and len(sys.argv) != 4:
@@ -14,22 +26,10 @@ if __name__ == "__main__":
     key_in = sys.argv[2]
 
     # Check if data_in is actually a path to an existing file
-    if os.path.isfile(data_in):
-        try:
-            with open(data_in, "r", encoding="utf-8") as f:
-                data_in = f.read()
-        except Exception as e:
-            print(f"Error reading file '{data_in}': {e}")
-            sys.exit(1)
+    data_in = _find_string(data_in)
 
     # Check if key_in is actually a path to an existing file
-    if os.path.isfile(key_in):
-        try:
-            with open(key_in, "r", encoding="utf-8") as f:
-                key_in = f.read()
-        except Exception as e:
-            print(f"Error reading file '{key_in}': {e}")
-            sys.exit(1)
+    key_in = _find_string(key_in)
 
     if len(sys.argv) == 3:
         print(encrypter_xor(string=data_in, key=key_in))
